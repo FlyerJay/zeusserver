ad7dd0c482a20edf70e3fa31a893e951a9fa5ab6
flyer <876173839@qq.com>
2017-04-18 20:11:16 +0800

    更新代码

--- app/router.js
+++ app/router.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = app => {
-  app.get('zues/api/', 'home.index');
-  app.post('zues/api/upload/excel', 'upload.excel');
-  app.post('zues/api/user/register', 'user.register');
-  app.post('zues/api/user/login', 'user.login');
-  app.get('zues/api/user/register', 'user.register');
-  app.get('zues/api/user/login', 'user.login');
+  app.get('/zues/api/', 'home.index');
+  app.post('/zues/api/upload/excel', 'upload.excel');
+  app.post('/zues/api/user/register', 'user.register');
+  app.post('/zues/api/user/login', 'user.login');
+  app.get('/zues/api/user/register', 'user.register');
+  app.get('/zues/api/user/login', 'user.login');
 };
