0fc97570e8e35b77e6ded47ce61dbde15835f077
flyerjay <876173839@qq.com>
2017-04-30 21:06:21 +0800

    更新购物车实体类

--- app/controller/chart.js
+++ app/controller/chart.js
@@ -4,7 +4,7 @@ module.exports = app => {
   class ChartController extends app.Controller {
     * list() {
       const ctx = this.ctx;
-      ctx.body = yield ctx.model.Chart.getList();
+      ctx.body = yield ctx.model.Chart.getList(ctx.request.body);
     }
   }
   return ChartController;
