d50c8621a8fa80f853e3fafbd0596255ab3239c9
flyerjay <876173839@qq.com>
2017-07-01 13:03:55 +0800

    更新定制化需求

--- app/controller/demand.js
+++ app/controller/demand.js
@@ -13,7 +13,7 @@ module.exports = app => {
     * add() {
       const ctx = this.ctx;
       const userRole = ctx.cookies.get('userRole');
-      if(userRole.charAt(5) === '0'){
+      if(userRole.charAt(4) === '0'){
         ctx.body = {
           code:-1,
           msg:"抱歉，没有权限进行该操作"
@@ -26,7 +26,7 @@ module.exports = app => {
       const ctx = this.ctx;
       const userRole = ctx.cookies.get('userRole');
       if(ctx.request.body.dealStatus){
-        if(userRole.charAt(5) === '0'){
+        if(userRole.charAt(4) === '0'){
           ctx.body = {
             code:-1,
             msg:"抱歉，没有权限进行该操作"
@@ -35,7 +35,7 @@ module.exports = app => {
           ctx.body = yield ctx.model.Demand.update(ctx.request.body);
         }
       }else{
-        if(userRole.charAt(4) === '0'){
+        if(userRole.charAt(5) === '0'){
           ctx.body = {
             code:-1,
             msg:"抱歉，没有权限进行该操作"
