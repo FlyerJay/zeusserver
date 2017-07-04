eac0aa8fecfe7a6ccdd577e4455c52069a3420d3
leolee815 <497523534@qq.com>
2017-07-04 15:42:13 +0800

    单支重量fix

--- app/view/src/components/supplier/stock.vue
+++ app/view/src/components/supplier/stock.vue
@@ -141,11 +141,11 @@ export default {
             const height = Number(specArr[0]);
             const width = Number(specArr[1]);
             const land = Number(specArr[2]);
-            const long = Number(row.long);
+            const long = Number(row.long) ? Number(row.long) : 6;
             const perimeter = 2 * height + 2 * width;
             const amount = Number(row.perAmount);
             const inventoryAmount = Number(row.inventoryAmount);
-            return ((perimeter / 3.14 - land) * land * 6 * 0.02466).toFixed(2);
+            return ((perimeter / 3.14 - land) * land * long * 0.02466).toFixed(2);
         },
         handleCurrentChange(val) {
             this.searchInvenParam.page = val;
