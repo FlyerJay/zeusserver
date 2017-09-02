80c9d4bb50612452e5441cf9abc2c6aa56a8de1d
flyerjay <876173839@qq.com>
2017-09-02 14:57:49 +0800

    输出日志

--- app/model/demand.js
+++ app/model/demand.js
@@ -140,7 +140,7 @@ module.exports = app => {
                     }
                 })
                 num ? submit += 1 : '';
-                console.log(app.io);
+                app.ctx.logger.info('socket.io信息：',app.io);
                 return new Promise((res,rej)=>{
                     app.io.in(`${options.comId}`).emit('update',{demand:{
                         submit,price,unDeal,deal
