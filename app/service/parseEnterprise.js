f095eb5836dcb470889749f494e6b27817a5558d
flyerjay <876173839@qq.com>
2020-03-03 15:45:12 +0800

    fix

--- app/service/parseEnterprise.js
+++ app/service/parseEnterprise.js
@@ -45,6 +45,47 @@ module.exports = app => {
             return options
         }
 
+        // 拆分数据
+        splitData (options) {
+            for(let i = 0; i < options.length; i++) {
+                var option = options[i]
+                option.lines.map((item, index) => {
+                    var itemArr = item.split(',')
+                    if ( itemArr[2]) {
+                        var append = itemArr[2].split(/\s+/)
+                        if(append.length === 1) {
+                            if(/\d{3,}/.test(append[0])) {
+                                var matchs = append[0].match(/(0\d{2,}-)?\d{7,}/)
+                                if (matchs && matchs[0]) {
+                                    append[0] = append[0].replace(matchs[0], '')
+                                    append[1] = matchs[0]
+                                }
+                            }
+                        }
+                        append.length = 2
+                        itemArr.splice(2, 1, ...append)
+                    }
+                    if (itemArr[4]) {
+                        var append = itemArr[4].split(/\s+/)
+                        if(append.length === 1) {
+                            if(/\d{3,}/.test(append[0])) {
+                                var matchs = append[0].match(/(0\d{2,}-)?\d{7,}/)
+                                if (matchs[0]) {
+                                    append[0] = append[0].replace(matchs[0], '')
+                                    append[1] = matchs[0]
+                                }
+                            }
+                        }
+                        append.length = 2
+                        itemArr.splice(4, 1, ...append)
+                    }
+                    itemArr.length = 6
+                    return option.lines[index] = itemArr
+                })
+            }
+            return options
+        }
+
         // 筛选出无效的数据
         invalidFilter (options) {
             return options.filter(item => {
